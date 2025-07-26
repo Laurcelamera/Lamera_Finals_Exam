f9d01d10fc9c0cba636bcdcf64bd6aebd699f2ec
Lau <38521979+Laurcelamera@users.noreply.github.com>
2025-07-26 16:16:48 +0800

    Add files via upload

---
+++ Data/Medicine.cs
@@ -0,0 +1,39 @@
+﻿using System.Security.AccessControl;
+using System.Xml.Linq;
+
+namespace Lamera_Midterms.Data
+{
+    public class Medicine
+    {
+        public string name { get; set; }
+        public string genericName { get; set; }
+        public double price {  get; set; }
+        public double quantity {  get; set; }
+        private double totalTransactionPrice;
+       
+        public Medicine()
+        {
+            name = "";
+            genericName = "";
+            price = 0;
+            quantity = 0;
+        }
+        public Medicine(string name, string genericName, double price, double quantity)
+        {
+            name = name;
+            genericName = genericName;
+            price = price;
+            quantity = quantity;
+        }
+        private void CalcTotalTransactionPrice(double quantity)
+        {
+            totalTransactionPrice = price * quantity;
+        }
+        public double GetTotalTransactionPrice(double quantity)
+        {
+            CalcTotalTransactionPrice(quantity);
+            return totalTransactionPrice;
+        }
+
+    }
+}
